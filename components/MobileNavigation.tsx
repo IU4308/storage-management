7ed33c057616e36f6c774c84b084bc80dacc97a4
Ilya <ilya671qw@gmail.com>
2024-11-12 15:16:53 +0300

    finished layout

---
+++ components/MobileNavigation.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
+const MobileNavigation = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MobileNavigation
